ec19a393f7c2009874ed1301b92d95d8ebb594b2
muffin <diegoxe7117@gmail.com>
2022-02-05 23:40:28 +1300

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,17 @@
+##
+# src/main.py
+# A program to simulate gravity, oh no
+
+import pygame
+
+screen_size = (600, 400)
+
+def main():
+    """
+    The main game loop
+    """
+    width, height = screen_size
+
+
+if __name__ == "__main__":
+    main()
